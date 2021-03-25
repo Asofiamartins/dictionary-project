1d5a2d9ce7366056311044747a709f2730317d9f
Asofiamartins <77688504+Asofiamartins@users.noreply.github.com>
2021-03-25 16:13:15 +0000

    add search engine

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-
+import React from 'react';
 import './App.css';
+import Footer from "./Footer";
+import Dictionary from "./Dictionary"
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
          <p>
           Dictionary
         </p>
-        
-      </header>
+        </header>
+        <main>
+          <Dictionary/>
+        </main>
+      <Footer/>
+    </div>
     </div>
   );
 }
 
-export default App;
+
---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css"
+
+
+
+export default function Footer(){
+
+    return (
+        <div className="footer">
+            <p> Open-source code by <a href="http://www.linkedin.com/in/asofia-martins" target="_blank" rel="noreferrer">
+        {" "}
+        Sofia Martins
+      </a></p>
+          
+        </div>
+    )
+}
